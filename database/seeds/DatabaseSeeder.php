f355fa22a60abc69ff1bb8ab6ef943bb08a7e799
andela-cvundi <christophervundi@gmail.com>
2016-02-10 22:06:47 +0300

    Configure blog title and number of blogs per page

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,18 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         // $this->call(UserTableSeeder::class);
+        $this->call('PostTableSeeder');
 
         Model::reguard();
+    }
+}
+
+class PostTableSeeder extends Seeder
+{
+    public function run()
+    {
+        App\Post::truncate();
+
+        factory(App\Post::class, 20)->create();
     }
 }
