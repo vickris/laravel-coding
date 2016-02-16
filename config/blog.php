ce8be8c8b830fdb0d9fbd25f20c905199358a6b9
andela-cvundi <christophervundi@gmail.com>
2016-02-16 15:11:15 +0300

    set storage to aws

--- config/blog.php
+++ config/blog.php
@@ -3,7 +3,7 @@ return [
     'title' => 'My Blog',
     'posts_per_page' => 5,
     'uploads' => [
-        'storage' => 'local',
-        'webpath' => '/uploads',
+        'storage' => 's3',
+        'webpath' => 'https://s3-us-west-2.amazonaws.com/laravel-coding',
     ],
 ];
