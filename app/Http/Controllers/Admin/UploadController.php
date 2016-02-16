f5d44e55b50de37cbf3fdacfb42cab0a8dba866b
andela-cvundi <christophervundi@gmail.com>
2016-02-16 11:10:34 +0300

    Update the index method for the Upload controller

--- app/Http/Controllers/Admin/UploadController.php
+++ app/Http/Controllers/Admin/UploadController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
-
+use App\Services\UploadsManager;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class UploadController extends Controller
 {
-    //
+    public function __construct(UploadsManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+    * Show page of files / subfolders
+    */
+    public function index(Request $request)
+    {
+        $folder = $request->get('folder');
+        $data = $this->manager->folderInfo($folder);
+
+        return view('admin.upload.index', $data);
+    }
 }
