8e54dfe33c4659c0572b0fc0e9560dd4ac12b404
andela-cvundi <christophervundi@gmail.com>
2016-02-22 11:22:33 +0300

    (fix) Link post model to tags model

--- app/Post.php
+++ app/Post.php
@@ -8,12 +8,77 @@ class Post extends Model
 {
     protected $dates = ['published_at'];
 
+    /**
+     * The many-to-many relationship between posts and tags.
+     *
+     * @return BelongsToMany
+     */
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag', 'post_tag_pivot');
+    }
+
+    /**
+     * Set the title attribute and automatically the slug
+     *
+     * @param string $value
+     */
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
 
         if (! $this->exists) {
-            $this->attributes['slug'] = str_slug($value);
+            $this->setUniqueSlug($value, '');
         }
+    }
+
+    /**
+     * Recursive routine to set a unique slug
+     *
+     * @param string $title
+     * @param mixed $extra
+     */
+    protected function setUniqueSlug($title, $extra)
+    {
+        $slug = str_slug($title.'-'.$extra);
+
+        if (static::whereSlug($slug)->exists()) {
+            $this->setUniqueSlug($title, $extra + 1);
+            return;
+        }
+
+        $this->attributes['slug'] = $slug;
+    }
+
+    /**
+     * Set the HTML content automatically when the raw content is set
+     *
+     * @param string $value
+     */
+    public function setContentRawAttribute($value)
+    {
+        $markdown = new Markdowner();
+
+        $this->attributes['content_raw'] = $value;
+        $this->attributes['content_html'] = $markdown->toHTML($value);
+    }
+
+    /**
+     * Sync tag relation adding new tags as needed
+     *
+     * @param array $tags
+     */
+    public function syncTags(array $tags)
+    {
+        Tag::addNeededTags($tags);
+
+        if (count($tags)) {
+            $this->tags()->sync(
+                Tag::whereIn('tag', $tags)->lists('id')->all()
+            );
+            return;
+        }
+
+        $this->tags()->detach();
     }
 }
