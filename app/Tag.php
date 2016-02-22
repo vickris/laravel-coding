0f3fd9f00b692dc5d7d3df13c17865d5b5497ad8
andela-cvundi <christophervundi@gmail.com>
2016-02-22 11:23:09 +0300

    Link tag model to post model in a many to many association

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,43 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $fillable = [
+    'tag', 'title', 'subtitle', 'page_image', 'meta_description',
+    'reverse_direction',
+    ];
+
+    /**
+    * The many-to-many relationship between tags and posts.
+    *
+    * @return BelongsToMany
+    */
+    public function posts()
+    {
+        return $this->belongsToMany('App\Post', 'post_tag_pivot');
+    }
+
+    /**
+    * Add any tags needed from the list
+    *
+    * @param array $tags List of tags to check/add
+    */
+    public static function addNeededTags(array $tags)
+    {
+        if (count($tags) === 0) {
+            return;
+        }
+
+        $found = static::whereIn('tag', $tags)->lists('tag')->all();
+
+        foreach (array_diff($tags, $found) as $tag) {
+            static::create([
+            'tag' => $tag,
+            'title' => $tag,
+            'subtitle' => 'Subtitle for '.$tag,
+            'page_image' => '',
+            'meta_description' => '',
+            'reverse_direction' => false,
+            ]);
+        }
+    }
 }
